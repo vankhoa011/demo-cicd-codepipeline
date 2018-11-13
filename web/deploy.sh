544d19621769b3e53d9593bea2c324ea71912f08
Khoa Nguyen <vankhoa011@gmail.com>
2018-11-13 15:05:34 +0700

    Change Docker Tag by Git Commit

--- web/deploy.sh
+++ web/deploy.sh
@@ -11,7 +11,7 @@ SERVICE_NAME="flask-app"
 TASK_FAMILY="flask-app"
 ECS_TASK_DEFINITION_TEMPLATE="task.json"
 ECS_TASK="task-ecs.json"
-ECR_IMAGE="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${IMAGE_NAME}:latest"
+ECR_IMAGE="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${IMAGE_NAME}:${CODEBUILD_RESOLVED_SOURCE_VERSION}"
 # Register Task Definition
 sed -e "s;%ECR_IMAGE%;${ECR_IMAGE};g" ${ECS_TASK_DEFINITION_TEMPLATE} > ${ECS_TASK}
 aws ecs register-task-definition --family ${TASK_FAMILY} --cli-input-json file://${ECS_TASK}
